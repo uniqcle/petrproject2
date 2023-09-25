3a2e17ca68dd35692e5fb865b11a24ba931be109
uniqcle <uniqcle@yandex.ru>
2023-09-25 10:08:25 +0700

    first

---
+++ dist/bundle.js
@@ -0,0 +1,13 @@
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+window.addEventListener('DOMContentLoaded', () => {
+  'use strict';
+
+  console.log('test');
+});
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
