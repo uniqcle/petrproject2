2f76be25fcb83ba28b15223560b63366f7ed9cdc
uniqcle <uniqcle@yandex.ru>
2023-09-27 17:09:04 +0700

    sliders

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,10 @@
 import modals from './modules/modals';
+import sliders from './modules/sliders';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict'
 
     modals();
-
+    sliders('.feedback-slider-item', 'horizontical', '.main-prev-btn', '.main-next-btn');
 
 })
