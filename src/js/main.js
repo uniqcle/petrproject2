9ddb23c897c18fd7960fd183b605bd51cc95398d
uniqcle <uniqcle@yandex.ru>
2023-10-18 16:57:02 +0700

    pictures sizes

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import sendForms from './modules/forms';
 import showMoreStyles from './modules/showMoreStyles';
 import calc from './modules/calc';
 import filter from './modules/filter';
+import pictureSize from './modules/pictureSize';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict'
@@ -15,4 +16,5 @@ window.addEventListener('DOMContentLoaded', () => {
     showMoreStyles('.button-styles', '#styles .row')
     calc('#size', '#material', '#options', '.promocode', '.calc-price')
     filter();
+    pictureSize('.sizes-block')
 })
